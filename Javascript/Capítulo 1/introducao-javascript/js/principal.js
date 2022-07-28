d14a287af52cfedd7985919bbe47f5450086c877
Peixee <Jc_px@hotmail.com>
2022-07-28 00:25:39 -0300

    aprendendo .textContent

---
+++ Javascript/Capítulo 1/introducao-javascript/js/principal.js
@@ -0,0 +1,5 @@
+console.log('Olá, mundo!')
+			var titulo = document.querySelector('h1');
+			titulo.textContent = "Peixe Personal"
+			var subtitulo = document.querySelector('.subtitulo')
+			subtitulo.textContent = "Clientes do site"
