cdc8fb8de260aed28037babd849322e2167f99a3
Peixee <Jc_px@hotmail.com>
2022-07-28 08:41:25 -0300

    capitulo 2 - trazendo info
    pegando informações do html, trazendo para js e mandando de volta para o html

--- Javascript/Capítulo 1/introducao-javascript/js/principal.js
+++ Javascript/Capítulo 1/introducao-javascript/js/principal.js
@@ -3,3 +3,22 @@ console.log('Olá, mundo!')
 			titulo.textContent = "Peixe Personal"
 			var subtitulo = document.querySelector('.subtitulo')
 			subtitulo.textContent = "Clientes do site"
+	
+var paciente = document.querySelector('#primeiro-paciente');
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector('.info-imc');
+
+
+var imc = peso/(altura*altura)
+tdImc.textContent = imc;
+
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+console.log(tdAltura);
+console.log(altura);
+console.log(imc)
